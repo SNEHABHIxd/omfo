93cc5371079e4aed599511c95e2e29f5116bd667
SNEHABHIxd <93216312+SNEHABHIxd@users.noreply.github.com>
2022-03-19 22:10:30 +0530

    Update Connect.py

--- Script/assistant/Keyboard/Connect.py
+++ Script/assistant/Keyboard/Connect.py
@@ -1,4 +1,6 @@
-from Script.assistant.TgCalls.Clients import abhi
+from Script.assistant.TgCalls.Clients import *
 
+with Client("Indian Music Bot", API_ID, API_HASH, bot_token=BOT_TOKEN) as app:
+    me_bot = app.get_me()
 with abhi as app:
     me_abhi = app.get_me()
